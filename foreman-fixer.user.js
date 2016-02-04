d4a94908161fb84af4e73020a94ef7a4c8a731ea
Alex Iribarren <Alex.Iribarren@cern.ch>
2016-02-04 13:49:30 +0100

    Ask for permissions

--- foreman-fixer.user.js
+++ foreman-fixer.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name       Foreman fixer
 // @namespace  http://cern.ch
-// @version    1.3
+// @version    1.4
 // @description  Fixes foreman's "X minutes ago" shit, plus adds "copy" buttons to Console Username and Password
 // @match      https://judy.cern.ch/*
 // @match      https://judy-ext.cern.ch/*
 // @match      https://foreman.cern.ch/*
 // @match      https://foreman-test.cern.ch/*
+// @grant      GM_setClipboard
+// @grant      GM_addStyle
 // @copyright  2013+, Alex Iribarren
 // @downloadURL https://raw.githubusercontent.com/alexiri/foreman-fixer/master/foreman-fixer.user.js
 // @require http://code.jquery.com/jquery-latest.min.js
