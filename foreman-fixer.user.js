7704cf09f3b314761906d8a01adc6ee7eaf2fc8a
Alex Iribarren <Alex.Iribarren@cern.ch>
2016-06-29 15:39:43 +0200

    Don't screw with column widths for all tables

--- foreman-fixer.user.js
+++ foreman-fixer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Foreman fixer
 // @namespace  http://cern.ch
-// @version    1.9
+// @version    1.10
 // @description  Fixes foreman's "X minutes ago" shit, plus adds "copy" buttons to Console Username and Password
 // @match      https://judy.cern.ch/*
 // @match      https://judy-ext.cern.ch/*
@@ -13,6 +13,20 @@
 // @downloadURL https://raw.githubusercontent.com/alexiri/foreman-fixer/master/foreman-fixer.user.js
 // @require http://code.jquery.com/jquery-latest.min.js
 // ==/UserScript==
+
+
+// Don't abbreviate stuff (particularly environment names)
+GM_addStyle('.ellipsis { overflow: initial; overflow-wrap: initial; white-space: initial; text-overflow: initial; }');
+
+// Compress lines a bit
+GM_addStyle('.btn-sm { line-height: 1 !important; }');
+
+GM_addStyle('.table-fixed { table-layout: auto; }');
+GM_addStyle('#content > table.table-fixed th:not(:first-child) { width: initial !important; }');
+GM_addStyle('#content > table.table-fixed th:nth-child(2) { width: 20% !important; }');
+GM_addStyle('#content > table.table-fixed th:nth-child(6) { width: 20% !important; }');
+GM_addStyle('#content > table.table-fixed th:last-child { width: 80px !important; }');
+
 
 function format(time) {
     var t = new Date(time);
@@ -37,51 +51,6 @@ function addCopy(item, text, value) {
             })
     );
 }
-
-var nameF = $("td:contains(Console Username)");
-if (nameF) {
-
-    username = nameF.siblings().text();
-
-    var pwdF = $("td:contains(Console Password)");
-    var password = pwdF.siblings().text();
-
-    addCopy(nameF.siblings()[0]);
-    addCopy(pwdF.siblings()[0]);
-
-    // Now make the Console link log in directly
-    var consoleF = $("a:contains(Console)");
-
-    if (consoleF.length > 0) {
-        var form = $('<form>')
-            .attr('target', '_blank')
-            .attr('method', 'post')
-            .css('display', 'none')
-            .attr('action', consoleF[0].href + 'cgi/login.cgi')
-            .append($('<input>').attr('name', 'name').attr('value', username).attr('type', 'hidden'))
-            .append($('<input>').attr('name', 'pwd').attr('value', password).attr('type', 'hidden'));
-
-        consoleF.parent().append(form);
-        consoleF.on('click', function (e) { form.submit(); e.preventDefault(); });
-
-        // Add the command-line
-        var host = consoleF[0].host;
-        var command = "ipmitool -I lanplus -U '" + username + "' -P '" + password + "' -H '" + host + "' sol activate";
-        addCopy(consoleF.parent()[0], "sol", command);
-    }
-}
-
-// Don't abbreviate stuff (particularly environment names)
-GM_addStyle('.ellipsis { overflow: initial; overflow-wrap: initial; white-space: initial; text-overflow: initial; }');
-
-// Compress lines a bit
-GM_addStyle('.btn-sm { line-height: 1 !important; }');
-
-GM_addStyle('.table-fixed { table-layout: auto; }');
-GM_addStyle('#content > table th:not(:first-child) { width: initial !important; }');
-GM_addStyle('#content > table th:nth-child(2) { width: 20% !important; }');
-GM_addStyle('#content > table th:nth-child(6) { width: 20% !important; }');
-GM_addStyle('#content > table th:last-child { width: 80px !important; }');
 
 // Hide incoming hostgroup
 var HIDE_INCOMING = 'hostgroup != incoming';
@@ -154,6 +123,40 @@ function changeStuff() {
             }
         }
     });
+
+    // Improve host details page
+    var nameF = $("td:contains(Console Username)");
+    if (nameF) {
+
+        username = nameF.siblings().text();
+
+        var pwdF = $("td:contains(Console Password)");
+        var password = pwdF.siblings().text();
+
+        addCopy(nameF.siblings()[0]);
+        addCopy(pwdF.siblings()[0]);
+
+        // Now make the Console link log in directly
+        var consoleF = $("a:contains(Console)");
+
+        if (consoleF.length > 0) {
+            var form = $('<form>')
+                .attr('target', '_blank')
+                .attr('method', 'post')
+                .css('display', 'none')
+                .attr('action', consoleF[0].href + 'cgi/login.cgi')
+                .append($('<input>').attr('name', 'name').attr('value', username).attr('type', 'hidden'))
+                .append($('<input>').attr('name', 'pwd').attr('value', password).attr('type', 'hidden'));
+
+            consoleF.parent().append(form);
+            consoleF.on('click', function (e) { form.submit(); e.preventDefault(); });
+
+            // Add the command-line
+            var host = consoleF[0].host;
+            var command = "ipmitool -I lanplus -U '" + username + "' -P '" + password + "' -H '" + host + "' sol activate";
+            addCopy(consoleF.parent()[0], "sol", command);
+        }
+    }
 }
 
 $(document).ready(function() { changeStuff(); });
